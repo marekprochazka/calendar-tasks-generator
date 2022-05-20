635668c61547066f75c16b14f5219a88af132a96
Marek Prochazka <m.prochazka2002@gmail.com>
2022-05-20 23:15:12 +0200

    init

---
+++ event.py
@@ -0,0 +1,84 @@
+
+import csv
+from typing import IO, TextIO
+
+
+class Date:
+    year: int = None
+    month: int = None
+    day: int = None
+
+    def __init__(self, day: int, month: int, year: int) -> None:
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def __str__(self) -> str:
+        return f"{self.day}/{str(self.month).zfill(2)}/{self.year}"
+
+    def add_days(self, days: int) -> None:
+        if self.month in (1, 3, 5, 7, 8, 10, 12):
+            if self.day + days > 31:
+                self.day = (self.day + days) % 31
+                self.month += 1
+            else:
+                self.day += days
+        elif self.month in (4, 6, 9, 11):
+            if self.day + days > 30:
+                self.day = (self.day + days) % 30
+                self.month += 1
+            else:
+                self.day += days
+        else:
+            if self.day + days > 28:
+                self.day = (self.day + days) % 28
+                self.month += 1
+            else:
+                self.day += days
+
+
+class Task:
+    name: str = None
+    description: str = None
+
+    def __init__(self, name: str, description: str) -> None:
+        self.name = name
+        self.description = description
+
+
+class TaskList:
+    dishes: Task = Task(
+        "Nádobí", "Každý večer si udělat čas a umít nádobí, které se nahromadilo v kuchyni, případně i vyčistit okolí uzná-li za vhodné.")
+    floor: Task = Task(
+        "Podlaha", "Na konci týdne, než odjede domů si udělá čas na to, aby umyl podlahu.")
+    hygiene_places: Task = Task(
+        "Záchod + koupelna", "Na konci týdne, než odjede domů si udělá čas na to, aby zkontroloval stav koupelny a záchodu a uzná-li za vhodné, tak vyčistí co je potřeba.")
+    vacuuming: Task = Task(
+        "Luxování", "Na konci týdne, než odjede domů si udělá čas na to, aby vyluxoval.")
+
+
+class Event:
+    start_date: Date = None
+    end_date: Date = None
+    task: Task = None
+    person: str = None
+
+    def __init__(self, start_date: Date, end_date: Date, task: Task, person: str) -> None:
+        self.start_date = start_date
+        self.end_date = end_date
+        self.task = task
+        self.person = person
+
+    def __generate_row(self) -> dict:
+        return {
+            "Subject": f'{self.person.upper()} - {self.task.name}',
+            "Start Date": str(self.start_date),
+            "Start Time": '',
+            "End Date": str(self.end_date),
+            "End Time": '',
+            "All day event": 'TRUE',
+            "Description": self.task.description,
+        }
+
+    def write_to_csv(self, file: csv.DictWriter) -> None:
+        file.writerow(self.__generate_row())
---
+++ generator.py
@@ -0,0 +1,56 @@
+from asyncio import events
+from tkinter import EventType
+from turtle import st
+from event import *
+from typing import List
+from itertools import cycle
+import copy
+import sys
+
+
+class Main:
+    start_date: Date = None
+    end_date: Date = None
+    num_weeks: int = None
+    persons: list = None
+    events: list = []
+
+    def __init__(self, start_date: Date, num_weeks: int, persons: list) -> None:
+        self.start_date = start_date
+        self.end_date = copy.deepcopy(start_date)
+        self.end_date.add_days(5)
+        self.num_weeks = num_weeks
+        self.persons = persons
+
+    def __generate_events(self) -> None:
+        person_iterator = cycle(self.persons)
+        for week in range(self.num_weeks):
+            self.events.append(Event(copy.deepcopy(self.start_date), copy.deepcopy(self.end_date), TaskList.dishes, next(person_iterator)))
+            self.events.append(Event(copy.deepcopy(self.start_date), copy.deepcopy(self.end_date), TaskList.dishes, next(person_iterator)))
+            self.events.append(Event(copy.deepcopy(self.start_date), copy.deepcopy(self.end_date), TaskList.floor, next(person_iterator)))
+            self.events.append(Event(copy.deepcopy(self.start_date), copy.deepcopy(self.end_date), TaskList.hygiene_places, next(person_iterator)))
+            self.events.append(Event(copy.deepcopy(self.start_date), copy.deepcopy(self.end_date), TaskList.vacuuming, next(person_iterator)))
+            self.start_date.add_days(7)
+            self.end_date.add_days(7)
+
+    def make_output(self) -> None:
+        self.__generate_events()
+        
+        with open('calendar_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            headers = ('Subject','Start Date','Start Time','End Date','End Time','All day event','Description')
+            writer = csv.DictWriter(csvfile, fieldnames=headers, delimiter=',')
+            writer.writeheader()
+            for event in self.events:
+                event.write_to_csv(writer)
+
+
+def main():
+    persons = ['Mara', 'Luke', 'Adam' ,'Skleny', 'Filda', 'Feela']
+    start_date = Date(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    num_weeks = int(int(sys.argv[4]))
+    main = Main(start_date, num_weeks, persons)
+    main.make_output()
+
+
+if __name__ == '__main__':
+    main()
